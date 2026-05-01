13f21f2e652ed9d79a679c056d83d35381b25228
Deewanshi Sharma <deewanshisharma24@gmail.com>
2026-05-01 20:35:20 +0530

    Update ProtectedRoute.js

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,15 +1,12 @@
-
-// src/ProtectedRoute.js
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({ children }) {
-  const token = localStorage.getItem('token'); // or however you store login state
-
-  if (!token) {
+export default function ProtectedRoute({ isLoggedIn, children }) {
+  // If there is no token/not logged in, redirect to the login page
+  if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
   }
 
+  // Otherwise, let them through to the component
   return children;
 }
-
-export default ProtectedRoute;
