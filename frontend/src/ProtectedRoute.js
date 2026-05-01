a19197449e23082fc2f36098c8144171cdf05960
Deewanshi Sharma <deewanshisharma24@gmail.com>
2026-05-01 20:28:17 +0530

    Update ProtectedRoute.js

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,1 +1,15 @@
 
+// src/ProtectedRoute.js
+import { Navigate } from 'react-router-dom';
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem('token'); // or however you store login state
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
